da4c2c902120f8a6b49105fbbad8d16a7ed946c3
glacambre <code@lacamb.re>
2018-02-14 14:54:13 +0100

    Add a few configuration things to test.zsh

--- test.zsh
+++ test.zsh
@@ -1,7 +1,7 @@
 
 dir="$(pwd)/$(dirname $0)"
-printf "aexport PATH=\"${dir}/python:\$PATH\"\nsource ${dir}/shell/nvim_shelley_zsh\n" | \
-	nvim -u NORC \
-		--cmd "source ${dir}/autoload/shelley.vim" \
-		--cmd "nnoremap <expr> b shelley#PrevPrompt()" \
-		term://"zsh -f"
+printf ":set rtp+=${dir}
+:nnoremap <expr> b shelley#PrevPrompt()
+:nnoremap <expr> a shelley#NextPrompt()
+:helptags doc
+isource ${dir}/shell/shelley.sh\n" | nvim -u NORC term://"zsh -f"
