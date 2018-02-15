810a427ed4e414f052c29e3582bbbf7b04464cb4
glacambre <code@lacamb.re>
2018-02-15 16:16:02 +0100

    Fix conditions

--- shell/shelley.zsh
+++ shell/shelley.zsh
@@ -12,7 +12,7 @@ if [[ ! -v precmd_functions ]]; then
 fi
 
 function shelley_man () {
-    if [[ "$SHELLEY_NOMAN" = "0" ]] ; then
+    if [[ "$SHELLEY_NOMAN" = "" || "$SHELLEY_NOMAN" = "0" ]] ; then
 	shelley --man $@
     else
 	command man $@
@@ -23,25 +23,25 @@ alias man='shelley_man'
 # automatically change working dir of parent buffer if using zsh within nvim
 function shelley_cd () {
     emulate -L zsh
-    if [[ "$SHELLEY_NOCD" = "0" ]] ; then
+    if [[ "$SHELLEY_NOCD" = "" || "$SHELLEY_NOCD" = "0" ]] ; then
 	shelley --cd "$(pwd)"
     fi
 }
-if [[ "$SHELLEY_NOCD" = "0" ]] ; then
+if [[ "$SHELLEY_NOCD" = "" || "$SHELLEY_NOCD" = "0" ]] ; then
     (shelley_cd &) >/dev/null
 fi
 chpwd_functions+=shelley_cd
 
 function shelley_preexec() {
     # Note: if this breaks, try ${{(%)${(e)PS1}}}
-    if [[ "$SHELLEY_NOPROMPT" = "0" ]] ; then
+    if [[ "$SHELLEY_NOPROMPT" = "" || "$SHELLEY_NOPROMPT" = "0" ]] ; then
 	shelley --preexec "$$" "${(%e)PS1}" "$1"
     fi
 }
 preexec_functions+=shelley_preexec
 
 function shelley_precmd() {
-    if [[ "$SHELLEY_NOPROMPT" = "0" ]] ; then
+    if [[ "$SHELLEY_NOPROMPT" = "" || "$SHELLEY_NOPROMPT" = "0" ]] ; then
 	shelley --precmd "$$"
     fi
 }
