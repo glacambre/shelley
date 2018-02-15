fb8521c0f0b932dfa273d3f7215c5f37733bf081
glacambre <code@lacamb.re>
2018-02-15 15:27:27 +0100

    Remove test.zsh

--- test.zsh
+++
@@ -1,7 +0,0 @@
-
-dir="$(pwd)/$(dirname $0)"
-printf ":set rtp+=${dir}
-:nnoremap <expr> b shelley#PrevPrompt()
-:nnoremap <expr> a shelley#NextPrompt()
-:helptags doc
-isource ${dir}/shell/shelley.sh\n" | nvim -u NORC term://"zsh -f"
